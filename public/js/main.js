8435f28edb7e7d7d803b8d952d8fd36c02a81d38
Alejandro <alejandroteutli@me.com>
2014-08-17 12:58:49 -0500

    Added functional buttons for deletion and task advancement. Refactored functions to be in global scope. Added initialization code inside document.ready()

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-  console.log( 'ready to roll!' );
+    console.log( 'ready to roll!' );
+    var ultimoTODO = $('.todo-el').last();
+    ultimoTODO.find('input').keydown(agregaNuevo);
 });
 //Setup
 
@@ -8,29 +10,36 @@ $(document).ready(function() {
 
 
 // TODO UI
-var ultimoTODO = $('.todo-el').last();
-var agregaNuevo = function (event){
-    console.log('ultimo apretado')
+
+function agregaNuevo(event){
     if(event.which == 13){
-	var nuevo = $('<input type="text" class="todo-el form-control" />');
-	nuevo.keydown(agregaNuevo);
+	var ultimoTODO = $(this).parents('.todo-el');
+	var nuevo = ultimoTODO.clone();
+	nuevo.find('input').keydown(agregaNuevo);
 	nuevo.appendTo('#todos');
-	ultimoTODO.unbind('keydown');
-	ultimoTODO.click(mandaTareaA('#doings',mandaTareaA('#dones', null)));
+	ultimoTODO.find('input').unbind('keydown');
+	ultimoTODO.find('.btn-success').click(mandaTareaA('#doings',mandaTareaA('#dones', null)));
+	ultimoTODO.find('.btn-danger').click(eliminaElemento);
 	ultimoTODO = nuevo;
 	
-	nuevo.focus();}
+	nuevo.find('input').focus();}
 }; 
-ultimoTODO.keydown(agregaNuevo);
 
-var mandaTareaA = function(columna, onClick){
-
+function mandaTareaA(columna, onClick){
     return function (event) {
-	console.log('mandando a' + columna);
-	$(this).detach()
-	.appendTo(columna)
-	.unbind('click')
-	.bind('click', onClick);
+	console.log('mandando a ' + columna);
+	$(this).parents('.todo-el').detach()
+	    .appendTo(columna)
+	    .find('.btn-success')
+	    .unbind('click')
+	    .bind('click', onClick);
     };
 };
+
+function eliminaElemento(){
+    console.log('Borrando elemento');
+    $(this).parents('.todo-el').remove();
+};
+
+
 
